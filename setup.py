2ffa052dc7f028aa4a2b0b7b6f237438a661a87a
Tom Bland <tom_bland@hotmail.co.uk>
2021-04-10 18:57:11 +0100

    Added requirements to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,17 @@ setup(
     version='1.0',
     license="MIT",
     author='Tom Bland',
-    packages=['membranequant']
+    packages=['membranequant'],
+    install_requires=[
+        'matplotlib~=3.3.3',
+        'numpy~=1.19.4',
+        'opencv-python==4.2.0.34 ',
+        'pandas==1.0.4',
+        'scipy~=1.4.1',
+        'scikit-image~=0.14.2',
+        'tensorflow~=2.4.0',
+        'tensorflow-probability==0.11.1',
+        'joblib==0.17.0',
+        'future~=0.18.2',
+        'tqdm~=4.55.0 ']
 )
