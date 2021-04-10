27a773f14901479e2b2e3b35b12b09e9ae854d3d
Tom Bland <tom_bland@hotmail.co.uk>
2021-04-10 18:33:07 +0100

    Added setup.py

--- membranequant/__init__.py
+++ membranequant/__init__.py
@@ -3,5 +3,5 @@ from .funcs import *
 from .gui import *
 from .interactive import *
 from .quantification import *
+from .quantification_method2 import *
 from .roi import *
-import pickle
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='membranequant',
+    version='1.0',
+    license="MIT",
+    author='Tom Bland',
+    packages=['membranequant']
+)
