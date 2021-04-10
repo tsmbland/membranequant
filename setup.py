cec38d4d9953b0b464d4d11469b3ce276828436f
Tom Bland <tom_bland@hotmail.co.uk>
2021-04-10 19:05:25 +0100

    Added requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+# Doesn't work yet. Doesn't seem to install all of the packages required by tensorflow
+
 setup(
     name='membranequant',
     version='1.0',
@@ -7,15 +9,15 @@ setup(
     author='Tom Bland',
     packages=['membranequant'],
     install_requires=[
-        'matplotlib~=3.3.3',
-        'numpy~=1.19.4',
+        'matplotlib==3.3.3',
+        'numpy==1.19.4',
         'opencv-python==4.2.0.34 ',
         'pandas==1.0.4',
-        'scipy~=1.4.1',
-        'scikit-image~=0.14.2',
-        'tensorflow~=2.4.0',
+        'scipy==1.4.1',
+        'scikit-image==0.14.2',
+        'tensorflow==2.4.0',
         'tensorflow-probability==0.11.1',
         'joblib==0.17.0',
-        'future~=0.18.2',
-        'tqdm~=4.55.0']
+        'future==0.18.2',
+        'tqdm==4.55.0']
 )
